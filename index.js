f99cb3d41abe5343cf3310786327bbce857a4cc1
ohryan <ryanachacon@gmail.com>
2020-09-03 15:31:39 -0600

    rename payloads to templates

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const changeCase = require('change-case');
 const settings = require('settings-store');
 const chalk = require('chalk');
 
-const payloads = require('./payloads');
+const templates = require('./templates');
 const { listFunc, readmeFunc, versionFunc, configFunc } = require('./lib');
 const getCommand = require('./lib/get-command');
 const setDefaults = require('./lib/set-defaults');
@@ -31,13 +31,13 @@ const initTemplateModule = async () => {
 
     if (!options._.length) reject(`\nNo arguments found. Run ${chalk.blue(`${commandUsed} --help`)} for more information.\n`);
 
-    // default naming for rsm payload
+    // default naming for rsm template
     if (type === 'rsm' && !name) {
       name = 'StateManager'
     }
 
-    // if no payload type was specified, default to 'main'
-    if (!payloads[type]) {
+    // if no template type was specified, default to 'main'
+    if (!templates[type]) {
       name = type
       type = 'main'
     }
@@ -46,11 +46,11 @@ const initTemplateModule = async () => {
   });
 };
 
-function createComponent({ type: payload, name }) {
+function createComponent({ type: template, name }) {
   const fileEncoding = { encoding: 'utf-8', flag: 'rs' };
 
   return new Promise(resolve => {
-    console.log(chalk.yellow(`Creating ${payload}-template: ${name}`))
+    console.log(chalk.yellow(`Creating ${template}-template: ${name}`))
     // make the initial parent directory
     try {
       execSync(`mkdir ${name}`);
@@ -58,20 +58,20 @@ function createComponent({ type: payload, name }) {
       console.log(chalk.red(`Directory already exists: ${name}. Overwriting...`))
     }
 
-    fs.readdirSync(path.join(__dirname, `/payloads/${payload}`)).forEach((file, i) => {
+    fs.readdirSync(path.join(__dirname, `/templates/${template}`)).forEach((file, i) => {
       const pascalName = changeCase.pascalCase(name);
       const camelName = changeCase.camelCase(name);
       const kebabName = changeCase.paramCase(name);
 
       console.log(chalk.blue(`Building: ${file.replace(/\$NAME\$/g, pascalName)} ${i}`))
 
-      // if folder in payload, create the folder
+      // if folder in template, create the folder
       const isFolder = file.lastIndexOf('.') === -1;
       if (isFolder) execSync(`mkdir ${pascalName}/${file.replace(/\$NAME\$/g, pascalName)}`);
       else execSync(`touch ./${pascalName}/${file.replace(/\$NAME\$/g, pascalName)}`);
 
       // read the data from the files and replace template names with argument name
-      const data = fs.readFileSync(path.join(__dirname, `/payloads/${payload}/${file}`), fileEncoding);
+      const data = fs.readFileSync(path.join(__dirname, `/templates/${template}/${file}`), fileEncoding);
       fs.writeFileSync(
         `./${name}/${file.replace(/\$NAME\$/g, pascalName)}`,
         data
