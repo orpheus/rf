ff211fff754f03a9aba1532c9211eba0e96c4d58
ohryan <ryanachacon@gmail.com>
2020-12-09 09:43:33 -0700

    update main gen

---
+++ templates/main/$NAME$.stories.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import $NAME$ from './$NAME$'
+
+export default {
+  title: '/$NAME$',
+  component: $NAME$,
+  argTypes: {},
+};
+
+const Template = (args) => <$NAME$ {...args} />
+
+export const Primary = Template.bind({})j
+Primary.args = {
+
+}
+
